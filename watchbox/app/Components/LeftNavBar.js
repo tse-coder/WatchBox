e0bee86c39ff54bf3582293d80ac58686378f4dd
tse-coder <shewamaretsegaye@gmail.com>
2025-03-29 13:28:17 +0300

    Update LeftNavBar.js

--- watchbox/app/Components/LeftNavBar.js
+++ watchbox/app/Components/LeftNavBar.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import categories from '../category'
 import { useAppContext } from '../context/AppContext'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 const Leftnavbar = () => {
   const { expanded, setExpanded, setCategory } = useAppContext();
@@ -13,12 +14,12 @@ const Leftnavbar = () => {
     <div className={`${expanded ? "block w-55 px-5" : "hidden w-15 px-2"} w-15 lg:block pt-3 fixed left-0 top-17 h-full bg-white text-gray-700 overflow-hidden border-r-2 border-r-gray-300 shrink-0 z-20 dark:bg-black dark:border-r dark:border-gray-500`}>
       <div className='border-b-2 border-b-gray-500 flex flex-col gap-4 pb-5'>
 
-        {categories.map(((cat, idx) => (
-          <a href='/' key={idx}>
+        {categories.map((cat, idx) => {
+          const content = (
             <div
               onClick={() => {
-                setCategory(cat.id)
-                setExpanded(false)
+                setCategory(cat.id);
+                setExpanded(false);
               }}
               className='flex gap-5 p-2 hover:shadow-lg hover:border-gray-100 hover:border-2 rounded-full cursor-pointer dark:text-gray-200 dark:hover:border dark:shadow-0'>
               <img
@@ -29,9 +30,10 @@ const Leftnavbar = () => {
               />
               <p className='font-semibold'>{cat.name}</p>
             </div>
-          </a>
+          );
 
-        )))}
+          return path !== '/' ? (<Link href="/">{content}</Link>) : (<div key={idx}>{content}</div>);
+        })}
       </div>
     </div>
   )
