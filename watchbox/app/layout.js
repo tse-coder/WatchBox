4c3a0ecb1cfc591e71bf6fa66a968a46dfce8252
tse-coder <shewamaretsegaye@gmail.com>
2025-03-29 12:27:06 +0300

    add metadata

--- watchbox/app/layout.js
+++ watchbox/app/layout.js
@@ -3,6 +3,17 @@ import Leftnavbar from "./Components/LeftNavBar";
 import "./globals.css";
 import { AppProvider } from "./context/AppContext";
 
+export const metadata = {
+  title: 'WatchBox',
+  description: 'a youtube clone made by tsegaye s.',
+  icons: {
+    icon: {
+       url: '/assets/logo.svg',
+       type: 'image/svg+xml'
+    }
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
